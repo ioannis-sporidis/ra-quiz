4c9a0e540363d7c174189dd4e1b506db75a753c9
isporidis <ioannis.sporidis@gmail.com>
2021-11-22 15:22:00 -0500

    Added next question functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Loading from "./components/Loading";
 import Modal from "./components/Modal";
 
 function App() {
-  const { waiting, loading, questions, index, correct } = useGlobalContext();
+  const { waiting, loading, questions, index, correct, nextQuestion } =
+    useGlobalContext();
 
   if (waiting) {
     return <SetupForm />;
@@ -38,7 +39,9 @@ function App() {
             })}
           </div>
         </article>
-        <button className="next-question">next question</button>
+        <button className="next-question" onClick={nextQuestion}>
+          next question
+        </button>
       </section>
     </main>
   );
